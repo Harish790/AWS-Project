25c081105fea8f35cdfa336f7415db9c0356406e
Harish Datta <60423913+Harish790@users.noreply.github.com>
2026-06-08 11:48:03 -0400

    Fix: Complete Cognito redirect URI in login.js to match uploadfile.js

--- login.js
+++ login.js
@@ -5,7 +5,7 @@ if (hash.includes("id_token")) {
   localStorage.setItem("token", token);
   history.replaceState(null, "", window.location.pathname);
 } else if (!localStorage.getItem("token")) {
-  window.location.href = "https://us-east-2hcni46ecv.auth.us-east-2.amazoncognito.com/login?client_id=j98ae55p06qf9r8lh59o1v7h9&response_type=token&scope=email+openid+profile&redirect_uri=https://harish790.github.io/AWS-Project/index11.html";
+  window.location.href = "https://us-east-2hcni46ecv.auth.us-east-2.amazoncognito.com/login?client_id=j98ae55p06qf9r8lh59o1v7h9&response_type=token&scope=email+openid+profile&redirect_uri=https://harishdatta.com/index.html";
 }
 
 function showLoginUI() {
@@ -21,7 +21,7 @@ function showUploadUI() {
 }
 
 function loginWithCognito() {
-  window.location.href = "https://us-east-2hcni46ecv.auth.us-east-2.amazoncognito.com/login?client_id=j98ae55p06qf9r8lh59o1v7h9&response_type=token&scope=email+openid+profile&redirect_uri=https://harish790.github.io/AWS-Project/index11.html";
+  window.location.href = "https://us-east-2hcni46ecv.auth.us-east-2.amazoncognito.com/login?client_id=j98ae55p06qf9r8lh59o1v7h9&response_type=token&scope=email+openid+profile&redirect_uri=https://harishdatta.com/index.html";
 }
 
 function logout() {
